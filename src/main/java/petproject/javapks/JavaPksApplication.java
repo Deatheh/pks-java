b44376ce19043858232f7f7d847c6c6c935fd2c8
Daniil <kazancevdanila2015@yandex.ru>
2026-09-05 11:25:30 +0300

    init: project

---
+++ src/main/java/petproject/javapks/JavaPksApplication.java
@@ -0,0 +1,13 @@
+package petproject.javapks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/petproject/javapks/JavaPksApplicationTests.java
@@ -0,0 +1,13 @@
+package petproject.javapks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
